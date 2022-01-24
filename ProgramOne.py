0f5e16db228ebda751f8d2be70b40c1944b2327f
Ekramul Hoque Ahnaf <85642191+ekram1320@users.noreply.github.com>
2022-01-24 12:29:45 +0600

    Add files via upload

---
+++ ProgramOne.py
@@ -0,0 +1,16 @@
+# print and string
+# Basic Python
+# operators
+print(10)
+print("Hello World")
+a = "Cumilla"
+b = 'University'
+c = "2022"
+hw = a + " " + b + " " + c
+print(hw)
+print("Cumilla" + " " + "University" + " " + "2022")
+x = 10
+print(x + 1)
+print(x - 1)
+print(x ** 2)
+print(x * 2)
